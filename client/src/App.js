bbe10deb385947aad784489f8a0f36532e7403a9
Charan Gautam <gautamsaicharan@yahoo.com>
2021-07-14 15:55:22 -0600

    app.js render pages logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Login from './pages/login/Login'
 import Home from './pages/home/Home'
 
+import {
+  BrowserRouter as Router, 
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom'
+import { AuthContext } from './contextAPI/AuthContext'
+
+
 function App() {
+  const { user } = useContext(AuthContext)
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          { user ? <Home /> : <Login /> }
+        </Route>
+        <Route path='/login'>
+          { user ? <Redirect to='/' /> : <Login />}
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './home.css'
 
+
 export default function Home() {
+    const handleClick = () => {
+        localStorage.clear()
+    }
+
     return (
         <div className='home'>
+            <button className='logoutButton' onClick={handleClick}>Log out </button>
             <div className="homeWrapper">
                 <div className="homeTop">
                     <span className='userHome'>Welcome, Chris Paul. Conquer the day!</span>
