ec30d42fbdf050b798b6f20ae284e95fb7759e05
Charan Gautam <gautamsaicharan@yahoo.com>
2021-07-14 12:56:20 -0600

    user state management

---
+++ client/src/contextAPI/AuthContext.js
@@ -0,0 +1,26 @@
+import { createContext, useReducer } from 'react'
+import AuthReducer from './AuthReducer'
+
+const initState = {
+    user: null,
+    isFetching: false,
+    error: false,
+}
+
+export const AuthContext = createContext(initState)
+
+
+export const AuthContextProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(AuthReducer, initState)
+
+    return (
+        <AuthContext.Provider value={{
+            user: state.user,
+            isFetching: state.isFetching,
+            error: state.error,
+            dispatch
+        }}>
+            { children }
+        </AuthContext.Provider>
+    )
+}
