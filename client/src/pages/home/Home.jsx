8800ca67631681367e95b5750c29c0494081d003
Charan Gautam <gautamsaicharan@yahoo.com>
2021-07-15 11:08:10 -0600

    home page functionality connected to backend server

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useContext, useRef } from 'react'
 import './home.css'
+import { AuthContext } from '../../contextAPI/AuthContext'
+import axios from 'axios'
 
 
 export default function Home() {
-    const handleClick = () => {
+    const { user } = useContext(AuthContext)
+    const firstName = useRef()
+    const lastName = useRef()
+    const UID = useRef()
+    const entryDate = useRef()
+    const profession = useRef()
+
+    const handleClick = (e) => {
         localStorage.clear()
+        window.location.reload()
+    }
+
+    const handleSubmit = async (e) => {
+        const post = {
+            firstName: firstName.current.value,
+            lastName: lastName.current.value,
+            UID: UID.current.value,
+            entryDate: entryDate.current.value,
+            profession: profession.current.value
+        }
+        try {
+            await axios.post('/posts', post)
+        } catch (err) {
+            console.log(err)
+        }
     }
 
     return (
@@ -12,36 +37,41 @@ export default function Home() {
             <button className='logoutButton' onClick={handleClick}>Log out </button>
             <div className="homeWrapper">
                 <div className="homeTop">
-                    <span className='userHome'>Welcome, Chris Paul. Conquer the day!</span>
+                    <span className='userHome'>Welcome {user.username}. Conquer the day!</span>
                 </div>
                 <div className='homeBottom'>
-                    <form className='dashboard'>
+                    <form className='dashboard' onSubmit={handleSubmit}>
                         <input 
                             className='dashboardItem'
                             type="text" 
                             placeholder='First Name'
+                            ref={firstName}
                         />
                         <input 
                             className='dashboardItem'
                             type="text" 
                             placeholder='Last Name'
+                            ref={lastName}
                         />
                         <input 
                             className='dashboardItem'
                             type="text" 
                             placeholder='UID'
+                            ref={UID}
                         />
                         <input 
                             className='dashboardItem'
                             type="date" 
                             placeholder='Date of Record Entry'
+                            ref={entryDate}
                         />
                         <input 
                             className='dashboardItem'
                             type="text" 
                             placeholder='Profession'
+                            ref={profession}
                         />
-                        <button className='dashboardButton'>Submit</button>
+                        <button className='dashboardButton' type='submit'>Submit</button>
                     </form>
                 </div>
             </div>
